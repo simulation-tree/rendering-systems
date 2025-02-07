ed1f825a87a1f1306577db39a6889bd5587fffe9
Phill <philldamaskin@gmail.com>
2025-02-06 20:39:39 -0500

    add missing viewport data

---
+++ core/Systems/ViewportData.cs
@@ -0,0 +1,16 @@
+﻿namespace Rendering.Systems
+{
+    public readonly struct ViewportData
+    {
+        public readonly Viewport entity;
+        public readonly LayerMask renderMask;
+        public readonly Destination destination;
+
+        public ViewportData(Viewport entity, LayerMask renderMask, Destination destination)
+        {
+            this.entity = entity;
+            this.renderMask = renderMask;
+            this.destination = destination;
+        }
+    }
+}
